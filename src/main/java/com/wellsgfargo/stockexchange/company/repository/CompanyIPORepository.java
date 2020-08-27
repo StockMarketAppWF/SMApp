a75a9ed10c3f201ac1f0e4f47349177d8f682a0a
Karthikeyan Iyer <karthik24iyer@gmail.com>
2020-08-27 16:08:34 +0530

    snehal pilot

---
+++ src/main/java/com/wellsgfargo/stockexchange/company/repository/CompanyIPORepository.java
@@ -0,0 +1,9 @@
+package com.wellsgfargo.stockexchange.company.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wellsgfargo.stockexchange.company.entity.CompanyIPO;
+
+public interface CompanyIPORepository extends JpaRepository<CompanyIPO, Integer> {
+
+}
