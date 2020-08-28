9676b1a9e876dbdbe92c743f46de32ba47bae865
Snehasis97 <43917712+Snehasis97@users.noreply.github.com>
2020-08-28 12:56:22 +0530

    Add files via upload

---
+++ src/main/java/com/wellsfargo/Sector.java
@@ -0,0 +1,36 @@
+package com.wellsfargo;
+
+public class Sector {
+	private String id,sector,desc;
+	public Sector() {
+	}
+	public Sector(String id,String sector, String desc) {
+		super();
+		this.id=id;
+		this.sector = sector;
+		this.desc = desc;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSector() {
+		return sector;
+	}
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	@Override
+	public String toString() {
+		return "Sector [id=" + id + ", sector=" + sector + ", desc=" + desc + "]";
+	}
+	
+}
