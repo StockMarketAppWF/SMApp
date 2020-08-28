ec494bb24eae7b9661062045e2d4f1c8a9c47c27
Snehasis97 <43917712+Snehasis97@users.noreply.github.com>
2020-08-28 11:58:07 +0530

    Revert "m1"

--- src/main/java/com/wellsgfargo/stockexchange/company/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wellsgfargo.stockexchange.company.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wellsgfargo.stockexchange.company.entity.Company;
-
-public interface CompanyRepository extends JpaRepository<Company,Integer> {
-
-}
