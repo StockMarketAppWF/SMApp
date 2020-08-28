b9cb46473f5b290542be2f81373ddbbd6a93f678
Karthikeyan Iyer <karthik24iyer@gmail.com>
2020-08-28 10:51:33 +0530

    Revert "snehal pilot"
    This reverts commit a75a9ed10c3f201ac1f0e4f47349177d8f682a0a.

--- src/main/java/com/wellsgfargo/stockexchange/company/repository/CompanyIPORepository.java
+++
@@ -1,9 +0,0 @@
-package com.wellsgfargo.stockexchange.company.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wellsgfargo.stockexchange.company.entity.CompanyIPO;
-
-public interface CompanyIPORepository extends JpaRepository<CompanyIPO, Integer> {
-
-}
