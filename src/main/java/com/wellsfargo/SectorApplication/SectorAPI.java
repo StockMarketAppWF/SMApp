46380efaff94fcb8c31d1971c16f8b798e655a88
Snehasis97 <43917712+Snehasis97@users.noreply.github.com>
2020-08-31 20:14:28 +0530

    Add files via upload

---
+++ src/main/java/com/wellsfargo/SectorApplication/SectorAPI.java
@@ -0,0 +1,11 @@
+package com.wellsfargo.SectorApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorAPI {
+	public static void main(String[] args) {
+		SpringApplication.run(SectorAPI.class, args);
+	}
+}
---
+++ src/test/java/com/wellsfargo/StockExchangeAPI/StockExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.StockExchangeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
