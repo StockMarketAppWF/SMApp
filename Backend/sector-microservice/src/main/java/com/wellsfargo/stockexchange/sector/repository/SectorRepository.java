74da07abca764d9b6abef9b18ac6e3b3dea2004f
Karthikeyan Iyer <karthik24iyer@gmail.com>
2020-09-08 19:12:15 +0530

    added both front and backend

--- Backend/sector-microservice/src/main/java/com/wellsfargo/stockexchange/sector/repository/SectorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wellsfargo.stockexchange.sector.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wellsfargo.stockexchange.sector.entity.Sector;
-
-public interface SectorRepository extends JpaRepository<Sector, String> {
-
-}
